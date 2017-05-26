f7134daa0a44dbfbdd5912af2f398fe9ea349175
RodrigoDeRosa <w8i9BtxBsrtgvb>
2017-05-26 13:29:23 -0300

    Cambiado pasto

--- Utils/ShaderSetUp.js
+++ Utils/ShaderSetUp.js
@@ -313,7 +313,13 @@ function getStreetRawFragmentShader(){
         if (vID == 0.0) textureColor = texture2D(streetTex, vec2(vTextureCoord.s, vTextureCoord.t));
         else if (vID == 1.0) textureColor = texture2D(crossTex, vec2(vTextureCoord.s, vTextureCoord.t));
         else if (vID == 2.0) textureColor = texture2D(sidewalkTex, vec2(vTextureCoord.s, vTextureCoord.t));
-        else if (vID == 3.0) textureColor = texture2D(grassTex, vec2(vTextureCoord.s, vTextureCoord.t));
+        else if (vID == 3.0){
+            //Se repite mas veces el pasto
+            vec2 auxUV = vTextureCoord;
+            auxUV.x = auxUV.x * 4.0;
+            auxUV.y = auxUV.y * 4.0;
+            textureColor = texture2D(grassTex, auxUV);
+        }
         else if (vID == 4.0) textureColor = texture2D(concreteTex, vec2(vTextureCoord.s, vTextureCoord.t));
         else if (vID == 5.0) textureColor = texture2D(asphaltTex, vec2(vTextureCoord.s, vTextureCoord.t));
         else if (vID == 6.0){
