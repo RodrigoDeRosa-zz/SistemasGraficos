cd6a9aeddf6256d317e65ed2558c58e830b08b27
RodrigoDeRosa <rodrigderosa@outlook.com>
2017-05-29 20:18:19 -0300

    Refactorizacion revolucion

--- SurfaceCreation/RevoltSurface.js
+++ SurfaceCreation/RevoltSurface.js
@@ -13,15 +13,15 @@ class RevoltSurface{
         var buffer = [];
         /*Para cada nivel de la revolucion se define cada punto de la forma*/
         for (var i = 0; i < this.levels; i++){
+            /*Se define la matriz de rotacion del nivel*/
+            var rotMat = mat4.create();
+            mat4.identity(rotMat);
+            //angulo = 2PI(nivelActua)/(nivelesTotales-1)
+            mat4.rotate(rotMat, rotMat, (Math.PI*2*i)/(this.levels-1), this.axis);
             /*Para cada punto de la forma se aplica la transformacion*/
             for (var j = 0; j < this.shape.getLevels(); j++){
                 var pos = this.shape.getPosition(j);
                 var vPos = vec3.fromValues(pos.x, pos.y, pos.z);
-                /*Se define la matriz de rotacion del nivel*/
-                var rotMat = mat4.create();
-                mat4.identity(rotMat);
-                //angulo = 2PI(nivelActua)/(nivelesTotales-1)
-                mat4.rotate(rotMat, rotMat, (Math.PI*2*i)/(this.levels-1), this.axis);
                 /*Se transforma el punto con la matriz del nivel*/
                 vec3.transformMat4(vPos, vPos, rotMat);
 
@@ -37,15 +37,15 @@ class RevoltSurface{
         var buffer = [];
         /*Para cada nivel se calcula la normal de cada punto de la forma*/
         for (var i = 0; i < this.levels; i++){
+            /*Matriz de rotacion del nivel*/
+            var rotMat = mat4.create();
+            mat4.identity(rotMat);
+            mat4.rotate(rotMat, rotMat, (Math.PI*2*i)/(this.levels-1), this.axis);
             /*Para cada punto de la forma se aplica la transformacion*/
             for (var j = 0; j < this.shape.getLevels(); j++){
                 var normal = this.shape.getNormal(j);
                 var vNormal = vec3.fromValues(normal.x, normal.y, normal.z);
 
-                /*Matriz de rotacion del nivel*/
-                var rotMat = mat4.create();
-                mat4.identity(rotMat);
-                mat4.rotate(rotMat, rotMat, (Math.PI*2*i)/(this.levels-1), this.axis);
                 /*Se transforma la normal con la matriz del nivel*/
                 vec3.transformMat4(vNormal, vNormal, rotMat);
 
